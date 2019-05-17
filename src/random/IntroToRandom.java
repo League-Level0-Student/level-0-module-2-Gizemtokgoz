9c892fd15ab6acc8e2607dfc2dbbe9faaa427ae7
League Student <league@iMac-18.attlocal.net>
2019-05-16 18:26:57 -0700

    Dice Roll, IntroToRandom, RockPaperScissors, Validation IP :smile:

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -13,12 +13,12 @@ public class IntroToRandom {
 		//3. Now make r random. 
 		//   Create an object of the Random class
 		//	 use .nextInt() to get a random number
-		
-
+		Random rand =new Random();
+		int randNumber = rand.nextInt(301) - 222;
 		//4. Limit the random number between 0 and 100
 
 		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
-
+			System.out.println(randNumber);
 		//6. Challenge: Limit the random number between -222 and 88
 
 		//1. Print out the value of r
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -10,16 +10,31 @@ import javax.swing.JOptionPane;
 public class Validation {
 	public static void main(String[] args) {
 		
-		Random randomMaker = new Random();
+		Random rand = new Random();
 		
-		int randomNumber = randomMaker.nextInt(5);
+		int randomChoice = rand.nextInt(5);
 		
-		System.out.println(randomNumber);
+		System.out.println(randomChoice);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		for (int i = 0; i < 10; i++) {
+			int randomChoice = rand.nextInt(5);
+		if (randomChoice == 0) {
+		
+		} else if (randomChoice == 1) {
+			JOptionPane.showMessageDialog(null, "You are O");
+		} else if (randomChoice == 2) {
+			JOptionPane.showMessageDialog(null, "You are Z");
+		} else if (randomChoice == 3) {
+			JOptionPane.showMessageDialog(null, "You are A");
+		} else if (randomChoice == 4) {
+			JOptionPane.showMessageDialog(null, "You are N");
+		} else {
+		
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
+		}
 	}
 }
